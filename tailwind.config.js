59d0738670104a5abf1e43e6c8d6095dae06d4de
CodingLikeAngel <angelnsz11@gmail.com>
2025-02-13 22:44:12 +0100

    added tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './libs/angel-ui-components/src/**/*.{html,ts,scss}', // Asegúrate de que esté incluido
+    './apps/**/*.{html,ts,scss}',               // También las apps si las estás usando
+  ],
   theme: {
     extend: {},
   },
