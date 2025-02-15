333b9f7151c96f1919b6e489094f3e20b2dcbe94
CodingLikeAngel <angelnsz11@gmail.com>
2025-02-15 03:04:34 +0100

    added non src project

---
+++ apps/test/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './../../../../libs/angel-ui-components/src/lib/angel-ui-components/header/header.component';
+import { FooterComponent } from './../../../../libs/angel-ui-components/src/lib/angel-ui-components/footer/footer.component';
+import { ChatComponent } from './../../../../libs/angel-ui-components/src/lib/angel-ui-components/chat/chat.component';
+// import { ContentGeneratorComponent } from './../../../../libs/angel-ui-components/src/lib/angel-ui-components/content-generator/content-generator.component';
+import { GeneratorComponent } from './../../../../libs/features/src/lib/features/generator/generator.component';
+
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  imports: [RouterModule, FooterComponent, HeaderComponent, ChatComponent, GeneratorComponent ],
+})
+export class AppComponent {
+  // Implementación del componente
+}
